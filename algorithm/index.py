cc81fb530d40af991481b536da18840583003a08
JaydenCheng <jaydencheng@whut.edu.cn>
2023-04-18 19:45:37 +0800

    增加删除已经建立的临时索引功能；增加实体字符串；增加操作返回值；修复无法新建索引的bug

--- algorithm/index.py
+++ algorithm/index.py
@@ -7,8 +7,11 @@ from elasticsearch.helpers import bulk
 class InvertedIndex:
     def __init__(self, client: Elasticsearch):
         self.client = client
-        self.index: Optional[str] = None
+        self.index: str = "co_occ_test"
         self.property: Optional[str] = None
+
+    def __repr__(self) -> str:
+        return f"Invert Index Instance {id(self):x} {self.client} {self.index}"
 
     _default_mapping = {
         "properties": {
@@ -25,13 +28,15 @@ class InvertedIndex:
             property_: str = "content",
             mappings=None,
     ):
+        report = None
         if mappings is None:
             mappings = InvertedIndex._default_mapping
-            self.client.create(
+            report = self.client.indices.create(
                 index=name, mappings=mappings
             )
         self.index = name
         self.property = property_
+        return report
 
     def _bulk_active(self, data: Iterable):
         for content in data:
@@ -47,7 +52,15 @@ class InvertedIndex:
             self,
             data: Iterable[str],
     ):
-        bulk(
+        report = bulk(
             self.client,
-            self._bulk_active(data)
+            self._bulk_active(data),
+            raise_on_error = False,
         )
+        return report
+
+    def delete_index_and_data(self):
+        report = self.client.indices.delete(index=self.index)
+        self.index = None
+        self.client = None
+        return report
